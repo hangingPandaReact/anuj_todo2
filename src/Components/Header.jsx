7e49824a8b6a84dbcd267f4eacae2d7149d5d4b4
anuj-hangingpanda <anujyadav3.hp@gmail.com>
2023-02-02 18:26:52 +0530

    Completed the  design part

---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export function Header() {
+  return (
+    <div className="header">
+      <h3>This is your Todo-list</h3>
+      <p>Write your all task here</p>
+    </div>
+  );
+}
