a03b5638a3ab272d9a0403cda75c333eabd1ff93
anuj-hangingpanda <anujyadav3.hp@gmail.com>
2023-02-04 19:01:01 +0530

    Delete task with Index also with id

--- src/Components/Border.jsx
+++ src/Components/Border.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Header } from "./Header";
-import { Todo } from "./Todolist";
+import { Todolist } from "./Todolist";
+// import { Todo } from "./Todolist";
 export function Border() {
   return (
     <div className="box">
       <Header />
-      <Todo />
+      <Todolist />
     </div>
   );
 }
--- src/Components/Todolist.jsx
+++ src/Components/Todolist.jsx
@@ -1,90 +1,95 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
-import addicon from "../add.png";
+import React, { useState } from "react";
 import { BsFillArchiveFill } from "react-icons/bs";
 import { BsFillPencilFill } from "react-icons/bs";
-import { useState } from "react";
-import { Alert } from "bootstrap";
 import { BsPlusLg } from "react-icons/bs";
 
-export function Todo() {
-  const [todo, setTodo] = useState("");
+export const Todolist = () => {
+  const [todo, setTodo] = useState({ test: "", id: "" });
   const [todoslist, settodoslist] = useState([]);
 
   console.log(todoslist, "todoslist");
 
+  // input handler
   const handleinput = (e) => {
-    setTodo(e.target.value);
+    setTodo({ test: e.target.value, id: new Date() });
   };
 
-  console.log(todo, "todo");
+  //Add todo task
 
   const addTodo = () => {
-    console.log(todo, "todo");
-    if (todo === "") alert("this is empty");
+    console.log(todo.test, "todo");
+    if (todo?.test === "") alert("this is empty");
     else {
       settodoslist([...todoslist, todo]);
     }
-    setTodo("");
+    setTodo({ test: "", id: "" });
   };
 
-  const deletTodo = (index) => {
-    const newTodos = todoslist.filter((todo) => {
-      return todo !== text;
+  //Delete task index
+  // const deletTodo = (index) => {
+  //   console.log(index, "getting");
+  //   const newTodos = todoslist.filter((todo, i) => {
+  //     return i !== index;
+  //   });
+  //   settodoslist(newTodos);
+  // };
+
+  //Delete task id
+  const deletTodo = (id) => {
+    console.log(id, "getting id");
+    const newTodos = todoslist.filter((newtodo, i) => {
+      return newtodo.id !== id;
     });
     settodoslist(newTodos);
   };
 
   return (
     <>
-      <input
-        className="input-txt"
-        type="test"
-        placeholder="Write your task here"
-        namer="todo"
-        value={todo}
-        onChange={(e) => handleinput(e)}
-      />
-      <button id="add-btn" onClick={addTodo}>
-        <BsPlusLg />
-      </button>
-
-      {todoslist?.length > 0 ? (
-        <ul className="todo-list">
-          {todoslist.map((text, index) => (
-            <div className="todo">
-              <input type="checkbox" className="custom-checkbox" />
-              <div className="txt">
-                <li className="listitem" key={index}>
-                  {text}
-                </li>
+      <div>
+        <input
+          className="input-txt"
+          type="test"
+          placeholder="Write your task here"
+          namer="todo"
+          value={todo.test}
+          onChange={(e) => handleinput(e)}
+        />
+        <button id="add-btn" onClick={addTodo}>
+          <BsPlusLg />
+        </button>
+      </div>
+      <div>
+        {todoslist?.length > 0 ? (
+          <ul className="todo-list">
+            {todoslist.map((text, index) => (
+              <div className="todo">
+                <input type="checkbox" className="custom-checkbox" />
+                <div className="txt">
+                  <li className="listitem" key={index}>
+                    {text?.test}
+                  </li>
+                </div>
+                <div className="btnd">
+                  <button className="edit-bt ">
+                    <BsFillPencilFill />
+                  </button>
+                  <button
+                    className="del-btn"
+                    id="del-btn"
+                    onClick={() => deletTodo(text.id)}
+                  >
+                    <BsFillArchiveFill />
+                  </button>
+                </div>
               </div>
-              <div className="btnd">
-                <button className="edit-bt ">
-                  <BsFillPencilFill />
-                </button>
-                <button
-                  className="del-btn"
-                  id="del-btn"
-                  onClick={(index) => {
-                    deleteTodo(index);
-                  }}
-                >
-                  <BsFillArchiveFill />
-                </button>
-              </div>
-            
-            </div>
-            
-          ))}
-        </ul>
-      ) : (
-        <div className="empty">
-          <p>Task list is Empty</p>
-        </div>
-      )}
+            ))}
+          </ul>
+        ) : (
+          <div className="empty">
+            <p>Task list is Empty</p>
+          </div>
+        )}
+      </div>
     </>
   );
-}
+};
